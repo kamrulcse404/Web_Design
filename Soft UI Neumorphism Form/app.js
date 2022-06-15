bd8e70a6ab691dfa582fbce7774de470a4e1c0b8
kamrulcse404 <kamrul.h01960@gmail.com>
2022-06-15 20:00:47 +0600

    new commit for wed design repository

---
+++ Soft UI Neumorphism Form/app.js
@@ -0,0 +1,16 @@
+// const btn = document.querySelector('button');
+
+// const inpt = document.querySelectorAll('input');
+
+// for(let i=0; i<inpt.length; i++){
+//     inpt[i].addEventListener(e, () => {
+//         if(e.target === value){
+//             inpt[i].style.background = 'none';
+//             inpt[i].style.border = 'none';
+//         }
+//     });
+// }
+
+// btn.addEventListener('click', () => {
+//     console.log('clicked');
+// });
